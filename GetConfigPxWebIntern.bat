63d760d2af1cc52c786252ee9c58a8e9589780c3
JohannesFinsveen <56305961+JohannesFinsveen@users.noreply.github.com>
2021-05-11 10:59:14 +0200

    [SS-474] Added script for copying configfiles

---
+++ GetConfigPxWebIntern.bat
@@ -0,0 +1,14 @@
+cd PXWeb\
+
+copy C:\git_adm.ssb.no\configRepos\px-web-intern\localhost\konfigfiler\databases.config .
+copy C:\git_adm.ssb.no\configRepos\px-web-intern\localhost\konfigfiler\setting.config .
+copy C:\git_adm.ssb.no\configRepos\px-web-intern\localhost\konfigfiler\SqlDb.config .
+copy C:\git_adm.ssb.no\configRepos\px-web-intern\localhost\konfigfiler\Web.config .
+
+rem cd PXWebIndeksering\
+rem mkdir utv_24v_intern
+rem copy C:\git_adm.ssb.no\config\px-web-intern\MetaIDConf\database.config utv_24v_intern\.
+rem copy C:\git_adm.ssb.no\config\px-web-intern\MetaIDConf\metadata.config utv_24v_intern\.
+echo "Done. Without PXWebIndeksering"
+
+
