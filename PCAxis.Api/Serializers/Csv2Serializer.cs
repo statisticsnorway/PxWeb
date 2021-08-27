26304823d798adcee278e27344ed63cfedf497c4
oyessb <oye@ssb.no>
2021-08-27 14:08:49 +0200

    [STATBANK-875] Added csv2 and csv3 serializer.

--- PCAxis.Api/Serializers/Csv2Serializer.cs
+++ PCAxis.Api/Serializers/Csv2Serializer.cs
@@ -24,9 +24,7 @@ namespace PCAxis.Api.Serializers
         {
             cacheResponse.ContentType = "text/csv; charset=" + System.Text.Encoding.Default.WebName;
 
-            //OBS OBS TODO Waiting for nu nuget
-            //PCAxis.Paxiom.IPXModelStreamSerializer serializer = new PCAxis.Paxiom.Csv2FileSerializer();
-            PCAxis.Paxiom.IPXModelStreamSerializer serializer = new PCAxis.Paxiom.CsvFileSerializer();
+            PCAxis.Paxiom.IPXModelStreamSerializer serializer = new PCAxis.Paxiom.Csv2FileSerializer();
 
             using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
             {
--- PCAxis.Api/Serializers/Csv3Serializer.cs
+++ PCAxis.Api/Serializers/Csv3Serializer.cs
@@ -12,9 +12,8 @@ namespace PCAxis.Api.Serializers
         public void Serialize(PCAxis.Paxiom.PXModel model, ResponseBucket cacheResponse)
         {
             cacheResponse.ContentType = "text/csv; charset=" + System.Text.Encoding.Default.WebName;
-            //OBS OBS TODO Waiting for nu nuget
-            //PCAxis.Paxiom.IPXModelStreamSerializer serializer = new PCAxis.Paxiom.Csv3FileSerializer();
-            PCAxis.Paxiom.IPXModelStreamSerializer serializer = new PCAxis.Paxiom.CsvFileSerializer();
+
+            PCAxis.Paxiom.IPXModelStreamSerializer serializer = new PCAxis.Paxiom.Csv3FileSerializer();
 
             using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
             {
