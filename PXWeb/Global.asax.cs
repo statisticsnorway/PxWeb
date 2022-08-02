9789c8668e08db2f1711d042df9fe7b5d888581c
Per Inge <piv@ssb.no>
2022-08-02 14:13:12 +0200

    [STATBANK-209] removed route to markingtips

--- PXWeb/Global.asax.cs
+++ PXWeb/Global.asax.cs
@@ -382,14 +382,6 @@ namespace PXWeb
                                            "{" + PxUrl.TABLE_KEY + "}/" +
                                            PxUrl.VIEW_INFORMATION_IDENTIFIER + "/",
                                            "~/InformationSelection.aspx");
-            RouteTable.Routes.MapPageRoute("SelectionTipsRoute",
-                                           PxUrl.PX_START + "/" +
-                                           "{" + PxUrl.LANGUAGE_KEY + "}/" +
-                                           "{" + PxUrl.DB_KEY + "}/" +
-                                           "{" + PxUrl.PATH_KEY + "}/" +
-                                           "{" + PxUrl.TABLE_KEY + "}/" +
-                                           PxUrl.VIEW_TIPS_IDENTIFIER + "/",
-                                           "~/MarkingTips.aspx");
             RouteTable.Routes.MapPageRoute("TablePresentationRoute",
                                            PxUrl.PX_START + "/" +
                                            "{" + PxUrl.LANGUAGE_KEY + "}/" +
