c20b80bbcbb8083653a69d6780dde89380c4eb2d
JohannesFinsveen <56305961+JohannesFinsveen@users.noreply.github.com>
2022-08-15 14:34:25 +0200

    [STATBANK-218] Missed a conflict.

--- PXWeb/Admin/Settings-Selection.aspx.cs
+++ PXWeb/Admin/Settings-Selection.aspx.cs
@@ -110,11 +110,8 @@ namespace PXWeb.Admin
                         sel.PreSelectFirstContentAndTime = bool.Parse(cboPreSelectFirstContentAndTime.SelectedValue);
                         sel.ShowNoFootnoteForSelection = bool.Parse(cboShowNoFootnoteForSelection.SelectedValue);
                         sel.SortVariableOrder = bool.Parse(cboSortVariableOrder.SelectedValue);
-<<<<<<< HEAD
                         sel.DefaultViewMode = (LayoutFormat)Enum.Parse(typeof(LayoutFormat), cboDefaultViewMode.SelectedValue.ToString());
-=======
                         sel.AlwaysShowCodeAndTextInAdvancedSearchResult = bool.Parse(cboAlwaysShowCodeAndTextInAdvancedSearchResult.SelectedValue);
->>>>>>> 0a4e8e9d435463f50be6c31a64695ac9c471a49c
 
                         PXWeb.Settings.Save();
                     }
@@ -311,15 +308,13 @@ namespace PXWeb.Admin
             Master.ShowInfoDialog("PxWebAdminSettingsSelectionSortVariableOrder", "PxWebAdminSettingsSelectionSortVariableOrderInfo");
         }
 
-<<<<<<< HEAD
         protected void DefaultViewModeInfo(object sender, ImageClickEventArgs e)
         {
             Master.ShowInfoDialog("PxWebAdminSettingsSelectionDefaultViewMode", "PxWebAdminSettingsSelectionDefaultViewModeInfo");
-=======
+
         protected void AlwaysShowCodeAndTextInAdvancedSearchResultInfo(object sender, ImageClickEventArgs e)
         {
             Master.ShowInfoDialog("PxWebAdminSettingsSelectionAlwaysShowCodeAndTextInAdvancedSearchResult", "PxWebAdminSettingsSelectionAlwaysShowCodeAndTextInAdvancedSearchResultInfo");
->>>>>>> 0a4e8e9d435463f50be6c31a64695ac9c471a49c
         }
     }
 }
